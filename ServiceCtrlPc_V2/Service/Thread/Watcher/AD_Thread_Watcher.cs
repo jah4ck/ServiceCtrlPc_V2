e7b303c6c81dd60b1cdc6a40cb7fedca1fdbccc4
jah4ck <johann.simon.web@gmail.com>
2017-06-30 00:58:34 +0200

    modification des nom de thread

--- ServiceCtrlPc_V2/Service/Thread/Watcher/AD_Thread_Watcher.cs
+++ ServiceCtrlPc_V2/Service/Thread/Watcher/AD_Thread_Watcher.cs
@@ -11,10 +11,13 @@ namespace Scheduler.Service.Thread.Watcher
     class AD_Thread_Watcher
     {
         private static string _Log_Src_Thread_Watcher;
+        private static string _Log_Src_Thread_Alert;
+        private static string __Log_Src_Thread_Watcher_Guid;
 
         public void Execute()
         {
-            _Log_Src_Thread_Watcher = CtrlPc_Service.Service_Log.Update(CtrlPc_Service.Service_Log_List, System.Threading.Thread.CurrentThread.Name.ToUpper(), "Watcher_" + System.Guid.NewGuid().ToString(), true);
+            __Log_Src_Thread_Watcher_Guid = System.Guid.NewGuid().ToString();
+            _Log_Src_Thread_Watcher = CtrlPc_Service.Service_Log.Update(CtrlPc_Service.Service_Log_List, System.Threading.Thread.CurrentThread.Name.ToUpper(), "Watcher_" + __Log_Src_Thread_Watcher_Guid, true);
 
             Scheduler.Tools.Log.AD_Logger_Tools.Log_Write("INFO", "Début Thread Watcher : Service_State = " + CtrlPc_Service.Service_State.ToUpper() + " / Service_Thread_Watcher_State = " + CtrlPc_Service.Service_Thread_Watcher_State.ToUpper());
 
@@ -52,44 +55,40 @@ namespace Scheduler.Service.Thread.Watcher
 
             foreach (string rep in CtrlPc_Service.Rep_Watcher)
             {
-                try
+                if (rep.ToUpper().Contains("UPDATE"))
                 {
-                    
-                    if (rep.ToUpper().Contains("UPDATE"))
+                    try
                     {
                         DirectoryInfo _Packages_Updates_Directory_Info = new System.IO.DirectoryInfo(rep);
                         FileSystemInfo[] _Packages_Updates_Files = _Packages_Updates_Directory_Info.GetFileSystemInfos("MEP_CTRLPC_*.EXE");
                         foreach (FileInfo _Packages_Updates_File in _Packages_Updates_Files)
                         {
-                            Tools.Log.AD_Logger_Tools.Log_Write("INFO", "Package de mise à jour trouvé : "+ _Packages_Updates_File.Name);
+                            Tools.Log.AD_Logger_Tools.Log_Write("INFO", "Package de mise à jour trouvé : " + _Packages_Updates_File.Name);
                             string _MD5_Ref = _Packages_Updates_File.Name.ToString().ToUpper().Replace("MEP_CTRLPC_", "").Replace(".EXE", "").ToLower();
                             string _MD5 = Tools.CheckSum.AD_CheckSum_Tools.GetFile_CheckSum("MD5", _Packages_Updates_File.FullName.ToString());
                             string _Package_Name = _Packages_Updates_File.Name.ToString().ToUpper();
                             if (_MD5_Ref == _MD5)
                             {
                                 Tools.Log.AD_Logger_Tools.Log_Write("INFO", "Le package trouvé est correct : " + _Packages_Updates_File.Name);
-                                Update.AD_Update_Service_Task.Update_Service_Task(_Packages_Updates_File.FullName,_Package_Name);
+                                Update.AD_Update_Service_Task.Update_Service_Task(_Packages_Updates_File.FullName, _Package_Name);
                             }
                             else
                             {
-                                Tools.Log.AD_Logger_Tools.Log_Write("WARN", "Le package trouvé est incorrect (différence de md5), fichier supprimé: " +_MD5+"<>"+_MD5_Ref);
+                                Tools.Log.AD_Logger_Tools.Log_Write("WARN", "Le package trouvé est incorrect (différence de md5), fichier supprimé: " + _MD5 + "<>" + _MD5_Ref);
                                 _Packages_Updates_File.Delete();
                             }
                         }
-
                     }
-                    
-                }
-                catch (Exception err)
-                {
-                    Tools.Log.AD_Logger_Tools.Log_Write("ERROR", err, new StackTrace(true));
-                }
-                try
-                {
-                    _Log_Src_Thread_Watcher = CtrlPc_Service.Service_Log.Update(CtrlPc_Service.Service_Log_List, System.Threading.Thread.CurrentThread.Name.ToUpper(), "Alert_" + System.Guid.NewGuid().ToString(), true);
-                    if (rep.ToUpper().Contains("ALERT"))
+                    catch (Exception _Exception)
                     {
-
+                        Tools.Log.AD_Logger_Tools.Log_Write("ERROR", _Exception, new StackTrace(true));
+                    }
+                }
+                if (rep.ToUpper().Contains("ALERT"))
+                {
+                    _Log_Src_Thread_Alert = CtrlPc_Service.Service_Log.Update(CtrlPc_Service.Service_Log_List, System.Threading.Thread.CurrentThread.Name.ToUpper(), "Alert_" + System.Guid.NewGuid().ToString(), true);
+                    try
+                    {
                         DirectoryInfo _Packages_Updates_Directory_Info = new System.IO.DirectoryInfo(rep);
                         FileSystemInfo[] _Files = _Packages_Updates_Directory_Info.GetFileSystemInfos("*_WAIT.txt");
                         foreach (FileInfo _File in _Files)
@@ -99,20 +98,22 @@ namespace Scheduler.Service.Thread.Watcher
                             string _File_Name = _File.Name.ToString().ToUpper();
                             if (_MD5_Ref == _MD5)
                             {
-                                Tools.Log.AD_Logger_Tools.Log_Write("INFO", "Le fichier trouvé est correct : " + _File.Name+", et prèt à être importé dans la bdd");
+                                Tools.Log.AD_Logger_Tools.Log_Write("INFO", "Le fichier trouvé est correct : " + _File.Name + ", et prèt à être importé dans la bdd");
                             }
                             else
                             {
-                                Tools.Log.AD_Logger_Tools.Log_Write("WARN", "Le fichier trouvé ("+ _File_Name + ") est incorrect (différence de md5), fichier supprimé: " + _MD5 + "<>" + _MD5_Ref);
+                                Tools.Log.AD_Logger_Tools.Log_Write("WARN", "Le fichier trouvé (" + _File_Name + ") est incorrect (différence de md5), fichier supprimé: " + _MD5 + "<>" + _MD5_Ref);
                                 _File.Delete();
                             }
                         }
                     }
+                    catch (Exception _Exception)
+                    {
+                        Tools.Log.AD_Logger_Tools.Log_Write("ERROR", _Exception, new StackTrace(true));
+                    }
                 }
-                catch (Exception _Exception)
-                {
-                    Tools.Log.AD_Logger_Tools.Log_Write("ERROR", _Exception, new StackTrace(true));
-                }
+                _Log_Src_Thread_Watcher = CtrlPc_Service.Service_Log.Update(CtrlPc_Service.Service_Log_List, System.Threading.Thread.CurrentThread.Name.ToUpper(), "Watcher_" + __Log_Src_Thread_Watcher_Guid, true);
+
             }
         }
     }
